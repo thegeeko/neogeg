298de0587dc509d603bb781a5f2933f47e54d904
Abdelhadi <abdelhadims@icloud.com>
2023-02-14 03:32:55 +0200

    added copy assets step + fix swapchain

--- engine/src/vulkan/swapchain.cpp
+++ engine/src/vulkan/swapchain.cpp
@@ -7,7 +7,8 @@ namespace geg::vulkan {
 		m_window = window;
 		m_device = device;
 
-		std::vector<vk::SurfaceFormatKHR> formats = m_device->physical_device.getSurfaceFormatsKHR(m_device->surface);
+		std::vector<vk::SurfaceFormatKHR> formats =
+				m_device->physical_device.getSurfaceFormatsKHR(m_device->surface);
 		GEG_CORE_ASSERT(!formats.empty(), "No surface formats available");
 
 		if (formats.size() == 1 && formats[0].format == vk::Format::eUndefined) {
@@ -20,32 +21,42 @@ namespace geg::vulkan {
 		vk::SurfaceCapabilitiesKHR surface_capabilities =
 				m_device->physical_device.getSurfaceCapabilitiesKHR(m_device->surface);
 
+		auto [width, height] = m_window->dimensions();
 		if (surface_capabilities.currentExtent.width == std::numeric_limits<uint32_t>::max()) {
 			m_extent.width = std::clamp(
-					m_window->width(), surface_capabilities.minImageExtent.width, surface_capabilities.maxImageExtent.width);
+					width,
+					surface_capabilities.minImageExtent.width,
+					surface_capabilities.maxImageExtent.width);
 			m_extent.height = std::clamp(
-					m_window->height(), surface_capabilities.minImageExtent.height, surface_capabilities.maxImageExtent.height);
+					height,
+					surface_capabilities.minImageExtent.height,
+					surface_capabilities.maxImageExtent.height);
 		} else {
 			m_extent = surface_capabilities.currentExtent;
 		}
 
 		m_present_mode = vk::PresentModeKHR::eFifo;
 
-		m_transform = (surface_capabilities.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity) ?
-											vk::SurfaceTransformFlagBitsKHR::eIdentity :
-											surface_capabilities.currentTransform;
+		m_transform =
+				(surface_capabilities.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity) ?
+						vk::SurfaceTransformFlagBitsKHR::eIdentity :
+						surface_capabilities.currentTransform;
 
 		m_composite_alpha =
-				(surface_capabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePreMultiplied) ?
+				(surface_capabilities.supportedCompositeAlpha &
+				 vk::CompositeAlphaFlagBitsKHR::ePreMultiplied) ?
 						vk::CompositeAlphaFlagBitsKHR::ePreMultiplied :
-				(surface_capabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePostMultiplied) ?
+				(surface_capabilities.supportedCompositeAlpha &
+				 vk::CompositeAlphaFlagBitsKHR::ePostMultiplied) ?
 						vk::CompositeAlphaFlagBitsKHR::ePostMultiplied :
 				(surface_capabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::eInherit) ?
 						vk::CompositeAlphaFlagBitsKHR::eInherit :
 						vk::CompositeAlphaFlagBitsKHR::eOpaque;
 
 		uint32_t image_count = std::clamp(
-				surface_capabilities.minImageCount + 1, surface_capabilities.minImageCount, surface_capabilities.maxImageCount);
+				surface_capabilities.minImageCount + 1,
+				surface_capabilities.minImageCount,
+				surface_capabilities.maxImageCount);
 
 		m_images.resize(image_count);
 
@@ -60,11 +71,16 @@ namespace geg::vulkan {
 				m_device->physical_device.getSurfaceCapabilitiesKHR(m_device->surface);
 
 		vk::Extent2D new_extent;
+		auto [width, height] = m_window->dimensions();
 		if (surface_capabilities.currentExtent.width == std::numeric_limits<uint32_t>::max()) {
 			new_extent.width = std::clamp(
-					m_window->width(), surface_capabilities.minImageExtent.width, surface_capabilities.maxImageExtent.width);
+					width,
+					surface_capabilities.minImageExtent.width,
+					surface_capabilities.maxImageExtent.width);
 			new_extent.height = std::clamp(
-					m_window->height(), surface_capabilities.minImageExtent.height, surface_capabilities.maxImageExtent.height);
+					height,
+					surface_capabilities.minImageExtent.height,
+					surface_capabilities.maxImageExtent.height);
 		} else {
 			new_extent = surface_capabilities.currentExtent;
 		}
